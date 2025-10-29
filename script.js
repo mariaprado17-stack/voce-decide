c8c244d03c1bfdf1f4c298d30fbbe2310c9e9401
mariaprado17-stack <maria.prado17@escola.pr.gov.br>
2025-10-29 08:39:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const avanca= document.querySelectorAll(`.btn-proximo`)
 
-avanca.forEach(button=>(
+avanca . forEach(Button=>{
+Button.addEventListener('click',function(){ 
+const atual =document.querySelector('ativo')
+const proximoPasso ='passo-'+ this.getAttribute('data-proxima')
+
+atual.classList.remove('ativo')
+document.getElementById(proximoPASSO).classList.add('ativo')
+   })
+})
+
+
+    
+
